ff9247807c06b5cb3d364709a3fc373b4ad7acf7
Damanus24 <121757777+Damanus24@users.noreply.github.com>
2024-02-08 19:15:10 +0300

    добавил доп. строку

--- src/main/java/ru/dauren/git_test/Test.java
+++ src/main/java/ru/dauren/git_test/Test.java
@@ -5,16 +5,6 @@ public class Test {
         System.out.println("Привет");
         System.out.println("Как дела?");
         System.out.println("Что делаешь?");
-        System.out.println("бла бла бла бла");
-        System.out.println("еще бла бла бла");
-        System.out.println("и снова бла бла бла");
-        System.out.println("и еще бла");
-        System.out.println("и опять еще бла");
-        System.out.println("бла бла");
-        System.out.println("аааааааааа");
-        System.out.println("bbbbbbbbbb");
-        System.out.println("cccccccccc");
-        System.out.println("dddddddddd");
-
+        System.out.println("Как здоровье?");
     }
 }
