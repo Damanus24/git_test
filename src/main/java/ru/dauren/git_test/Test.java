b0418fdb40cb49378bf6a5737a6c6b24e9a46e33
Damanus24 <121757777+Damanus24@users.noreply.github.com>
2024-02-08 19:15:10 +0300

    общий коммит после squash

---
+++ src/main/java/ru/dauren/git_test/Test.java
@@ -0,0 +1,9 @@
+package ru.dauren.git_test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        System.out.println("Как дела?");
+        System.out.println("Что делаешь?");
+    }
+}
