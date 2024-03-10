4823eef987e177fa8a912839959f62d423c5824e
Damanus24 <121757777+Damanus24@users.noreply.github.com>
2024-03-10 15:25:49 +0300

    + доп. строка

--- src/main/java/ru/dauren/git_test/Test.java
+++ src/main/java/ru/dauren/git_test/Test.java
@@ -5,6 +5,20 @@ public class Test {
         System.out.println("Привет");
         System.out.println("Как дела?");
         System.out.println("Что делаешь?");
+        System.out.println("Что делаешь?");
+        System.out.println("Что делаешь?");
+        System.out.println("Что делаешь?");
+        System.out.println("Что делаешь?");
+        System.out.println("Что делаешь?");
         System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaa");
+        int a = 5;
+        if (a > 3) {
+            System.out.println("sad");
+        } else if (a > 5) {
+            
+        }
     }
 }
